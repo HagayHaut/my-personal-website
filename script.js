441e1d08e31c9d762a835b19131a4ccebf49338e
Hagay Haut <hagayhaut@gmail.com>
2022-03-04 10:14:14 -0500

    add section w/ js

---
+++ script.js
@@ -0,0 +1,5 @@
+const newDiv = document.createElement('div');
+const content = document.createTextNode('Email at hagayhaut@haut.com for inquiries');
+newDiv.appendChild(content);
+var newElement = document.getElementById('js-section');
+newElement.appendChild(newDiv);
